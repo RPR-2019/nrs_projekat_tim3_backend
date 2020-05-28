38cb4b8302f1ba5bdec4b7ffadb92db47c9a1241
Bakir Karovic <bakir.karovic@gmail.com>
2020-05-28 22:37:08 +0200

    optimized imports

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const authChecks = require("../authChecks.js");
-const passport = require("passport");
 const flash = require("express-flash");
 const connection = require("../database.js");
 const queries = require("../queries.js");
