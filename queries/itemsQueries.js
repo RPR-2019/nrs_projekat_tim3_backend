b3156960bd3e2e058afe9aa6d9ed88af90bd122b
Bakir Karovic <bakir.karovic@gmail.com>
2020-05-29 00:01:45 +0200

    added crud queries for items

---
+++ queries/itemsQueries.js
@@ -0,0 +1,83 @@
+var queries = (function () {
+  function getItemsImpl(connection, callback) {
+    connection.query("SELECT * FROM proizvodi", function (
+      error,
+      results,
+      fields
+    ) {
+      if (error) throw error;
+      callback(results);
+    });
+  }
+
+  function getItemByIdImpl(connection, id, callback) {
+    connection.query("SELECT * FROM proizvodi where id = ?", [id], function (
+      error,
+      results
+    ) {
+      if (error) throw error;
+      callback(results[0]);
+    });
+  }
+
+  function deleteItemByIdImpl(connection, id, callback) {
+    getItemByIdImpl(connection, id, (data) => {
+      if (data === undefined || data === null) {
+        callback(1);
+      } else {
+        let query = "DELETE FROM proizvodi WHERE id=" + id;
+        connection.query(query, callback);
+      }
+    });
+  }
+
+  function updateItemByIdImpl(connection, item, callback) {
+    let query = "UPDATE proizvodi SET ";
+    let params = [];
+    if (item.naziv !== undefined) {
+      query += "naziv=?,";
+      params.push(item.naziv);
+    }
+    //enables null to be passed
+    if (item.proizvodjac !== undefined) {
+      query += "proizvodjac=?,";
+      params.push(item.proizvodjac);
+    }
+    if (item.kategorija !== undefined) {
+      query += "kategorija=?";
+      params.push(item.kategorija);
+    }
+    params.push(item.id);
+    if (query.slice(-1) == ",") {
+      query = query.substring(0, query.length - 1);
+    }
+    query += " WHERE id=?";
+    connection.query(query, params, (error, results, fields) => {
+      if (error) {
+        callback(null, 1);
+      } else {
+        getItemByIdImpl(connection, item.id, callback);
+      }
+    });
+  }
+
+  function addItemImpl(connection, item, callback) {
+    let query =
+      "INSERT INTO proizvodi (naziv, proizvodjac, kategorija) VALUES (?,?,?)";
+    connection.query(
+      query,
+      [item.naziv, item.proizvodjac, item.kategorija],
+      callback
+    );
+  }
+
+  return {
+    getItems: getItemsImpl,
+    getItemById: getItemByIdImpl,
+    deleteItemById: deleteItemByIdImpl,
+    updateItemById: updateItemByIdImpl,
+    addItem: addItemImpl,
+  };
+})();
+
+module.exports = queries;
