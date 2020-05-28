c602979d9f24a25f394e458ca7103ab7cff508f2
Bakir Karovic <bakir.karovic@gmail.com>
2020-05-28 21:29:10 +0200

    users crud operations done

--- queries.js
+++ queries.js
@@ -1,26 +1,90 @@
-var queries = function () {
-    async function getUsersImpl(connection, callback) {
-        connection.query('SELECT * FROM korisnicki_racuni', function (error, results, fields) {
-            if (error) throw error;
-            callback(results);
-        });
-    }
+var queries = (function () {
+  async function getUsersImpl(connection, callback) {
+    connection.query("SELECT * FROM korisnicki_racuni", function (
+      error,
+      results,
+      fields
+    ) {
+      if (error) throw error;
+      callback(results);
+    });
+  }
 
-    function getUserByIdImpl(connection, id, callback) {
-        connection.query(
-            'SELECT * FROM korisnicki_racuni where id = ?',
-            [id],
-            function (error, results) {
-                if (error) throw error;
-                callback(null, results[0]);
+  function getUserByIdImpl(connection, id, callback) {
+    connection.query(
+      "SELECT * FROM korisnicki_racuni where id = ?",
+      [id],
+      function (error, results) {
+        if (error) throw error;
+        callback(null, results[0]);
+      }
+    );
+  }
 
-            });
-    }
+  function getPersonByIdImpl(connection, id, callback) {
+    connection.query(
+      "SELECT k.pravo_pristupa, k.email, o.ime, o.prezime, o.telefon, o.datum_zaposljavanja, o.JMBG, o.naziv_lokacije FROM korisnicki_racuni k RIGHT JOIN osobe o ON (o.id= k.osoba_id) where k.id = ?",
+      [id],
+      function (error, results) {
+        if (error) throw error;
+        callback(results[0]);
+      }
+    );
+  }
 
-    return {
-        getUsers: getUsersImpl,
-        getUserById: getUserByIdImpl
+  function deleteUserByIdImpl(connection, id, callback) {
+    queries.getUserById(connection, id, (temp, data) => {
+      if (data === undefined || data === null) {
+        callback(1);
+      } else {
+        let query = "DELETE FROM korisnicki_racuni WHERE id=" + id;
+        connection.query(query, callback);
+      }
+    });
+  }
+
+  function updatePersonByIdImpl(connection, params, callback) {
+    let query =
+      "UPDATE osobe SET ime=?, prezime=?, telefon=?, datum_zaposljavanja=?, JMBG=?, naziv_lokacije=? WHERE id=?";
+    connection.query(query, params, callback);
+  }
+
+  function updateUserByIdImpl(connection, user, callback) {
+    let query = "UPDATE korisnicki_racuni SET ";
+    let params = [];
+    if (user.password) {
+      query += "password=?,";
+      params.push(user.password);
     }
-}();
+    if (user.email) {
+      query += "email=?,";
+      params.push(user.email);
+    }
+    if (user.pravo_pristupa) {
+      query += "pravo_pristupa=?,";
+      params.push(user.pravo_pristupa);
+    }
+    if (query.slice(-1) == ",") {
+      query = query.substring(0, query.length - 1);
+    }
+    params.push(user.id);
+    query += "WHERE id=?";
+    connection.query(query, params, (error, results, fields) => {
+      if (error) {
+        console.log(error);
+        throw error;
+      }
+      getUserByIdImpl(connection, user.id, callback);
+    });
+  }
+
+  return {
+    getUsers: getUsersImpl,
+    getUserById: getUserByIdImpl,
+    getPersonById: getPersonByIdImpl,
+    deleteUserById: deleteUserByIdImpl,
+    updateUserById: updateUserByIdImpl,
+  };
+})();
 
 module.exports = queries;
--- server.js
+++ server.js
@@ -1,95 +1,98 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const flash = require('express-flash');
-const session = require('express-session');
-const methodOverride = require('method-override');
-const helmet = require('helmet')
+const expressLayouts = require("express-ejs-layouts");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const helmet = require("helmet");
 // const https = require('https');
-const fs = require('fs');
+const fs = require("fs");
 // const http = require('http');
-let net = require('net');
-let io = require('socket.io');
-let httpx = require('./httpx');
+let net = require("net");
+let io = require("socket.io");
+let httpx = require("./httpx");
 
-
-const initializePassport = require('./passport-config');
-const authChecks = require('./authChecks.js');
-const connection = require('./database.js');
-const { ROLE } = require('./roles.js');
-const queries = require('./queries.js');
+const initializePassport = require("./passport-config");
+const authChecks = require("./authChecks.js");
+const connection = require("./database.js");
+const { ROLE } = require("./roles.js");
+const queries = require("./queries.js");
 
 const options = {
-
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
 };
 
 initializePassport(
-    passport,
-    async function (email, callback) {
-        console.log("email" + email);
-        queries.getUsers(
-            connection,
-            function (data) {
-                callback(
-                    data.find(function (user) {
-                        return user.email === email;
-                    })
-                )
-            }
-            //data => data.find(user => user.email == email)
-        )
-    },
-    function (id, callback) {
-        queries.getUserById(
-            connection,
-            id,
-            callback
-        )
-    }
-)
-
-
-
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
-app.set('layout', 'layouts/layout');
-app.set('views', __dirname + '/views')
-app.use(express.urlencoded({ extended: false }));
-app.use(flash());
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(methodOverride('_method'));
-app.use(express.json());
-app.use(helmet());
-//app.use(helmet.xssFilter())
-
-app.all("/", authChecks.checkAuthenticated, require('./routes/index'));
-
-app.all(["/login", "/login*"], authChecks.checkNotAuthenticated, require('./routes/login'));
-app.all("/logout", authChecks.checkAuthenticated, require('./routes/login'));
-
-app.all(["/register", "/register*"], authChecks.checkNotAuthenticated, require('./routes/register'));
-
-app.all(
-    ["/users", "/users*"],
-    authChecks.checkAuthenticated,
-    authChecks.authRole(ROLE.ADMIN),
-    require('./routes/users')
+  passport,
+  async function (email, callback) {
+    console.log("email" + email);
+    queries.getUsers(connection, function (data) {
+      callback(
+        data.find(function (user) {
+          return user.email === email;
+        })
+      );
+    });
+  },
+  function (id, callback) {
+    queries.getUserById(connection, id, callback);
+  }
 );
 
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("layout", "layouts/layout");
+app.set("views", __dirname + "/views");
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+app.use(express.json());
+app.use(helmet());
+
+app.all("/", authChecks.checkAuthenticated, require("./routes/index"));
+
+app.all(
+  ["/login", "/login*"],
+  authChecks.checkNotAuthenticated,
+  require("./routes/login")
+);
+app.all("/logout", authChecks.checkAuthenticated, require("./routes/login"));
+
+app.all(
+  ["/register", "/register*"],
+  authChecks.checkNotAuthenticated,
+  require("./routes/register")
+);
+
+app.all(
+  ["/users", "/users*"],
+  //authChecks.checkAuthenticated,
+  //authChecks.authRole(ROLE.ADMIN),
+  require("./routes/users")
+);
+
+app.all(
+  ["/people", "/people*"],
+  //authChecks.checkAuthenticated,
+  //authChecks.authRole(ROLE.ADMIN),
+  require("./routes/people")
+);
 
 //app.listen(process.env.PORT || 8080);
 
@@ -112,7 +115,7 @@ http.createServer(
 
 let server = httpx.createServer(options, app);
 let ws = io(server.http);
-let wss = io(server.https)
-server.listen(process.env.PORT || 8080, '0.0.0.0');
+let wss = io(server.https);
+server.listen(process.env.PORT || 8080, "0.0.0.0");
 
 console.log("Server started. Listening on port 8080");
