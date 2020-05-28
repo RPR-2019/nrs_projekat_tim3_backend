a90f0cbea32fde8663a14ff63717f8d1bc9d43bf
Bakir Karovic <bakir.karovic@gmail.com>
2020-05-29 00:17:12 +0200

    implemented warehouses router

---
+++ routes/warehouses.js
@@ -0,0 +1,137 @@
+const express = require("express");
+const router = express.Router();
+const authChecks = require("../authChecks.js");
+const flash = require("express-flash");
+const connection = require("../database.js");
+const queries = require("../queries/warehousesQueries.js");
+const { ROLE } = require("../roles.js");
+var htmlEncode = require("js-htmlencode").htmlEncode;
+
+router.get("/warehouses", (req, res) => {
+  queries.getWarehouses(connection, (data) => res.json(data));
+});
+
+router.get(
+  //TODO - add html render
+  "/warehouses/add",
+  authChecks.checkAuthenticated,
+  authChecks.authRole(ROLE.ADMIN),
+  async (req, res) => {
+    res.render("addUser.ejs");
+  }
+);
+
+router.get(
+  "/warehouses/:id",
+  //authChecks.checkAuthenticated,
+  //authChecks.authRole(ROLE.ADMIN),
+  (req, res) => {
+    queries.getWarehouseById(connection, req.params.id, (data) => {
+      if (data == undefined || data == null) {
+        res.writeHead("404");
+        res.write(JSON.stringify({ error: "Warehouse not found" }));
+      } else {
+        res.writeHead("200");
+        res.write(JSON.stringify(data));
+      }
+      res.send();
+    });
+  }
+);
+
+router.delete(
+  "/warehouses/:id",
+  //authChecks.checkAuthenticated,
+  //authChecks.authRole(ROLE.ADMIN),
+  (req, res) => {
+    queries.deleteWarehouseById(
+      connection,
+      req.params.id,
+      (error, results, fields) => {
+        if (error) {
+          res.writeHead(500);
+          res.write(
+            JSON.stringify({ error: "Warehouse not found or has dependency" })
+          );
+        } else {
+          res.writeHead(200);
+          res.write(JSON.stringify({ success: "Warehouse deleted" }));
+        }
+        res.send();
+      }
+    );
+  }
+);
+
+router.put(
+  "/warehouses/:id",
+  //authChecks.checkAuthenticated,
+  //authChecks.authRole(ROLE.ADMIN),
+  async (req, res) => {
+    let warehouse = {};
+    warehouse.id = req.params.id;
+    if (req.body.naziv !== undefined) {
+      warehouse.naziv = htmlEncode(req.body.naziv);
+    }
+    if (req.body.naziv_lokacije !== undefined) {
+      req.body.naziv_lokacije !== null
+        ? (warehouse.naziv_lokacije = htmlEncode(req.body.naziv_lokacije))
+        : (warehouse.naziv_lokacije = req.body.naziv_lokacije);
+    }
+    queries.updateWarehouseById(connection, warehouse, (data, error) => {
+      if (error) {
+        res.writeHead("404");
+        res.write(JSON.stringify({ error: "Error" }));
+      } else if (data == undefined || data == null) {
+        res.writeHead("404");
+        res.write(JSON.stringify({ error: "Warehouse not found" }));
+      } else {
+        res.writeHead("200");
+        res.write(JSON.stringify(data));
+      }
+      res.send();
+    });
+  }
+);
+
+router.post("/warehouses/add", async (req, res) => {
+  try {
+    let warehouse = {};
+    if (req.body.naziv !== undefined) {
+      warehouse.naziv = htmlEncode(req.body.naziv);
+    }
+    if (req.body.naziv_lokacije !== undefined) {
+      req.body.naziv_lokacije !== null
+        ? (warehouse.naziv_lokacije = htmlEncode(req.body.naziv_lokacije))
+        : (warehouse.naziv_lokacije = req.body.naziv_lokacije);
+    }
+    queries.addWarehouse(connection, warehouse, function (
+      error,
+      results,
+      fields
+    ) {
+      if (error) {
+        console.log(error);
+        res.writeHead(500);
+        res.write(JSON.stringify({ error: "Error" }));
+        res.send();
+      } else {
+        queries.getWarehouseById(connection, results.insertId, (data) => {
+          if (data == undefined || data == null) {
+            res.writeHead("404");
+            res.write(JSON.stringify({ error: "Warehouse not found" }));
+          } else {
+            res.writeHead("200");
+            res.write(JSON.stringify(data));
+          }
+          res.send();
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
