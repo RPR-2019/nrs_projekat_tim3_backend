a5547901ef0fbd3de79c5b78d5ce9cf2dcddcbe1
Bakir Karovic <bakir.karovic@gmail.com>
2020-05-29 23:51:38 +0200

    added call for checkAll function that checks if passed values are valid

--- queries/orderItemsQueries.js
+++ queries/orderItemsQueries.js
@@ -6,48 +6,57 @@ var queries = (function () {
   function getOrderItemsByIdImpl(id, callback) {
     ordersQ.getOrderById(id, (data) => {
       if (data == null) {
-        callback(null, 1);
+        callback(1);
       } else {
         connection.query(
           "SELECT * from artikli_narudzbe WHERE narudzba_id=?",
           [id],
-          function (error, results, fields) {
-            if (error) throw error;
-            callback(results);
-          }
+          callback
         );
       }
     });
   }
 
   function addOrderItemsImpl(orderId, itemId, quantity, supplierId, callback) {
-    ordersQ.getOrderById(orderId, (data) => {
-      if (data == null) {
-        callback(1);
-      } else {
-        supplierItemsQ.getSupplierItemById(
-          supplierId,
-          itemId,
-          (error, data) => {
-            if (error) {
-              callback(error);
-            } else if (data == null) {
-              callback(1);
-            } else {
-              connection.query(
-                "INSERT INTO proizvodi_skladista(proizvod_id, kolicina, dobavljac_id, narudzba_id)" +
-                  " VALUES(?,?,?,?)",
-                [itemId, quantity, supplierId, orderId],
-                callback
-              );
-            }
-          }
-        );
-      }
+    checkAll(orderId, itemId, supplierId, callback, () => {
+      connection.query(
+        "INSERT INTO proizvodi_skladista(proizvod_id, kolicina, dobavljac_id, narudzba_id)" +
+          " VALUES(?,?,?,?)",
+        [itemId, quantity, supplierId, orderId],
+        callback
+      );
     });
   }
 
   function deleteOrderItemsByIdImpl(orderId, itemId, supplierId, callback) {
+    checkAll(orderId, itemId, supplierId, callback, () => {
+      connection.query(
+        "DELETE FROM proizvodi_skladista " +
+          "WHERE narudzba_id=? and proizvod_id=?",
+        [orderId, itemId],
+        callback
+      );
+    });
+  }
+
+  function updateOrderItemsByIdImpl(
+    orderId,
+    itemId,
+    quantity,
+    supplierId,
+    callback
+  ) {
+    checkAll(orderId, itemId, supplierId, callback, () => {
+      connection.query(
+        "UPDATE proizvodi_skladista SET kolicina=?" +
+          "WHERE narudzba_id=? AND proizvod_id=? AND dobavljac_id=?",
+        [quantity, orderId, itemId, supplierId],
+        callback
+      );
+    });
+  }
+
+  function checkAll(orderId, itemId, supplierId, callback, resolve) {
     ordersQ.getOrderById(orderId, (data) => {
       if (data == null) {
         callback(1);
@@ -66,18 +75,13 @@ var queries = (function () {
                   callback(1);
                 } else {
                   connection.query(
-                    "SELECT * FROM proizvodi_skladista WHERE skladiste_id=? AND proizvod_id=?",
+                    "SELECT * FROM proizvodi_skladista WHERE narudzba_id=? AND proizvod_id=?",
                     [orderId, itemId],
                     (error, results) => {
                       if (results[0] == null) {
                         callback(2);
                       } else {
-                        connection.query(
-                          "DELETE FROM proizvodi_skladista " +
-                            "WHERE skladiste_id=? and proizvod_id=?",
-                          [orderId, itemId],
-                          callback
-                        );
+                        resolve();
                       }
                     }
                   );
