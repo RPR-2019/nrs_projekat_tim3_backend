9c50584250d3c9c2a6c0977cae7a3f640d1aa052
Bakir Karovic <bakir.karovic@gmail.com>
2020-05-31 20:27:22 +0200

    removed redundant commented code

--- routes/purchaseItems.js
+++ routes/purchaseItems.js
@@ -7,102 +7,82 @@ const queries = require("../queries/purchaseItemsQueries.js");
 const { ROLE } = require("../roles.js");
 var htmlEncode = require("js-htmlencode").htmlEncode;
 
-router.get(
-  "/purchases/:id/items",
-  //authChecks.checkAuthenticated,
-  //authChecks.authRole(ROLE.ADMIN),
-  (req, res) => {
-    queries.getPurchaseItemsById(req.params.id, (error, results) => {
-      if (error) {
+router.get("/purchases/:id/items", (req, res) => {
+  queries.getPurchaseItemsById(req.params.id, (error, results) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+router.post("/purchases/:id/items/:itemId", (req, res) => {
+  let purchaseId = req.params.id;
+  let itemId = req.params.itemId;
+  let quantity = req.body.quantity;
+  if (!quantity || !itemId || !purchaseId) {
+    res.json({ error: "Invalid params." });
+    return;
+  }
+  quantity = htmlEncode(quantity);
+  queries.addPurchaseItems(purchaseId, itemId, quantity, function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      if (error.error) {
         res.json(error);
       } else {
-        res.json(results);
+        res.json({
+          error: "Purchase already has that item.",
+        });
       }
-    });
-  }
-);
-
-router.post(
-  "/purchases/:id/items/:itemId",
-  //authChecks.checkAuthenticated,
-  //authChecks.authRole(ROLE.ADMIN),
-  (req, res) => {
-    let purchaseId = req.params.id;
-    let itemId = req.params.itemId;
-    let quantity = req.body.quantity;
-    if (!quantity || !itemId || !purchaseId) {
-      res.json({ error: "Invalid params." });
-      return;
+    } else {
+      res.json({ success: "Item added to purchase." });
     }
-    quantity = htmlEncode(quantity);
-    queries.addPurchaseItems(purchaseId, itemId, quantity, function (
-      error,
-      results,
-      fields
-    ) {
-      if (error) {
-        if (error.error) {
-          res.json(error);
-        } else {
-          res.json({
-            error: "Purchase already has that item.",
-          });
-        }
-      } else {
-        res.json({ success: "Item added to purchase." });
-      }
-    });
-  }
-);
+  });
+});
 
-router.delete(
-  "/purchases/:id/items/:itemId",
-  //authChecks.checkAuthenticated,
-  //authChecks.authRole(ROLE.ADMIN),
-  (req, res) => {
-    let purchaseId = req.params.id;
-    let itemId = req.params.itemId;
-    if (!itemId || !purchaseId) {
-      res.json({ error: "Invalid params." });
-      return;
-    }
-    queries.deletePurchaseItemById(purchaseId, itemId, function (
-      error,
-      results,
-      fields
-    ) {
-      if (error) {
-        res.json(error);
-      } else {
-        res.json({ success: "Purchase item deleted" });
-      }
-    });
+router.delete("/purchases/:id/items/:itemId", (req, res) => {
+  let purchaseId = req.params.id;
+  let itemId = req.params.itemId;
+  if (!itemId || !purchaseId) {
+    res.json({ error: "Invalid params." });
+    return;
   }
-);
+  queries.deletePurchaseItemById(purchaseId, itemId, function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ success: "Purchase item deleted" });
+    }
+  });
+});
 
-router.delete(
-  "/purchases/:id/items",
-  //authChecks.checkAuthenticated,
-  //authChecks.authRole(ROLE.ADMIN),
-  (req, res) => {
-    let purchaseId = req.params.id;
-    if (!purchaseId) {
-      res.json({ error: "Invalid params." });
-      return;
-    }
-    queries.deleteAllPurchaseItemsById(purchaseId, function (
-      error,
-      results,
-      fields
-    ) {
-      if (error) {
-        res.json(error);
-      } else {
-        res.json({ success: "Purchase items deleted" });
-      }
-    });
+router.delete("/purchases/:id/items", (req, res) => {
+  let purchaseId = req.params.id;
+  if (!purchaseId) {
+    res.json({ error: "Invalid params." });
+    return;
   }
-);
+  queries.deleteAllPurchaseItemsById(purchaseId, function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ success: "Purchase items deleted" });
+    }
+  });
+});
 
 router.put(
   "/purchases/:id/items/:itemId",
