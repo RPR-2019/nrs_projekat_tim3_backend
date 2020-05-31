426d338bb5ba649b35284c71a4e116a3d923085f
Bakir Karovic <bakir.karovic@gmail.com>
2020-05-31 20:22:15 +0200

    added check if purchase belogns to buyer when updating

--- routes/purchases.js
+++ routes/purchases.js
@@ -82,36 +82,57 @@ router.put(
     if (body.apy_key === process.env.API_KEY) {
       user = body.korisnicki_racun;
     } else {
-      user = req.user.id;
+      user = req.user;
+      if (!undefinedAndCheck(user, body.stanje_id)) {
+        res.json({ error: "Wrong params" });
+        return;
+      }
+      user = user.id;
+      if (!undefinedAndCheck(user)) {
+        res.json({ error: "Wrong params" });
+        return;
+      }
     }
-    if (undefinedAndCheck(user, body.stanje_id)) {
-      res.json({ error: "Wrong params" });
-      return;
-    }
+
     let purchase = {};
     purchase.id = req.params.id;
-    user !== null
+    user != null
       ? (purchase.korisnicki_racun = htmlEncode(user))
       : (purchase.korisnicki_racun = user);
 
-    body.stanje_id !== null
+    body.stanje_id != null
       ? (purchase.stanje_id = htmlEncode(body.stanje_id))
       : (purchase.stanje_id = body.stanje_id);
-    queries.updatePurchaseById(purchase, (error, data) => {
-      if (error) {
-        res.writeHead("404");
-        res.write(JSON.stringify({ error: "User or condition not found!" }));
-      } else if (data == undefined || data == null) {
-        res.writeHead("404");
-        res.write(JSON.stringify({ error: "Purchase not found" }));
-      } else {
-        res.writeHead("200");
-        res.write(JSON.stringify(data));
-      }
-      res.send();
-    });
+    if (body.apy_key !== process.env.API_KEY) {
+      await queries.getPurchaseById(req.params.id, (error, results) => {
+        if (!req.user || results[0].korisnicki_racun != user) {
+          res.json({ error: "You are not allowed to delete that purchase" });
+          return;
+        } else {
+          updatePurchase(purchase, res);
+        }
+      });
+    } else {
+      updatePurchase(purchase, res);
+    }
   }
 );
+
+function updatePurchase(purchase, res) {
+  queries.updatePurchaseById(purchase, (error, data) => {
+    if (error) {
+      res.writeHead("404");
+      res.write(JSON.stringify({ error: "User or condition not found!" }));
+    } else if (data == null) {
+      res.writeHead("404");
+      res.write(JSON.stringify({ error: "Purchase not found" }));
+    } else {
+      res.writeHead("200");
+      res.write(JSON.stringify(data));
+    }
+    res.send();
+  });
+}
 
 router.post("/purchases", async (req, res) => {
   var body = req.body;
